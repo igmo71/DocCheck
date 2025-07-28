66985b32baaa14a9b8bd07ec52f5b1a05445eb73
Igor Mogilnitskiy <igmo71@outlook.com>
2025-07-28 12:45:09 +0300

    004 Create messages if fail.

--- DocCheck/Data/ApplicationUser.cs
+++ DocCheck/Data/ApplicationUser.cs
@@ -5,5 +5,8 @@ namespace DocCheck.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        //public string? FirstName { get; set; }
+        //public string? MiddleName { get; set; }
+        //public string? LastName { get; set; }
     }
 }
