6561f36def03a53d29c76633d90cb078075c9510
Igor Mogilnitskiy <igmo71@outlook.com>
2025-09-01 11:43:39 +0300

    Add Services.

---
+++ DocCheck/Data/Migrations/20250901083142_Initialize.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocCheck.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BitrixId",
+                table: "AspNetUsers",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MiddleName",
+                table: "AspNetUsers",
+                type: "nvarchar(36)",
+                maxLength: 36,
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "SaleDocLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    IsCorrect = table.Column<bool>(type: "bit", nullable: false),
+                    SaleDocId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Log = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SaleDocLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SaleDocLogs_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SaleDocs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Position = table.Column<int>(type: "int", nullable: false),
+                    Redispatch = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SaleDocs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SaleDocs_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PaperworkErrors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SaleDocId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaperworkErrors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PaperworkErrors_SaleDocs_SaleDocId",
+                        column: x => x.SaleDocId,
+                        principalTable: "SaleDocs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuantityErrors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SaleDocId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LineNumber = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<double>(type: "float", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuantityErrors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuantityErrors_SaleDocs_SaleDocId",
+                        column: x => x.SaleDocId,
+                        principalTable: "SaleDocs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaperworkErrors_SaleDocId",
+                table: "PaperworkErrors",
+                column: "SaleDocId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuantityErrors_SaleDocId",
+                table: "QuantityErrors",
+                column: "SaleDocId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleDocLogs_UserId",
+                table: "SaleDocLogs",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SaleDocs_UserId",
+                table: "SaleDocs",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaperworkErrors");
+
+            migrationBuilder.DropTable(
+                name: "QuantityErrors");
+
+            migrationBuilder.DropTable(
+                name: "SaleDocLogs");
+
+            migrationBuilder.DropTable(
+                name: "SaleDocs");
+
+            migrationBuilder.DropColumn(
+                name: "BitrixId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "MiddleName",
+                table: "AspNetUsers");
+        }
+    }
+}
--- DocCheck/Program.cs
+++ DocCheck/Program.cs
@@ -1,9 +1,18 @@
+using DocCheck.Application;
 using DocCheck.Components;
 using DocCheck.Components.Account;
 using DocCheck.Data;
+using DocCheck.Infrastructure.Bitrix;
+using DocCheck.Infrastructure.OData;
+using DocCheck.Infrastructure.RabbitMq;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using System.Net.Http.Headers;
+using System.Text;
+using Bitrix = DocCheck.Infrastructure.Bitrix;
+using OData = DocCheck.Infrastructure.OData;
 
 namespace DocCheck
 {
@@ -13,7 +22,12 @@ namespace DocCheck
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .CreateLogger();
+
+            builder.Services.AddSerilog();
+
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
@@ -28,8 +42,9 @@ namespace DocCheck
                     options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
                 })
                 .AddIdentityCookies();
-
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -39,7 +54,46 @@ namespace DocCheck
                 .AddSignInManager()
                 .AddDefaultTokenProviders();
 
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Default Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+            });
+
             builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+            var odataConfig = builder.Configuration.GetSection(nameof(OData)).Get<ODataConfig>()
+                ?? throw new InvalidOperationException("OData Config Not Found");
+            var authenticationString = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{odataConfig.UserName}:{odataConfig.Password}"));
+            builder.Services.AddHttpClient<ODataClient>(httpClient =>
+            {
+                httpClient.BaseAddress = new Uri(odataConfig.BaseAddress);
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authenticationString);
+            });
+
+            var bitrixConfig = builder.Configuration.GetSection(nameof(Bitrix)).Get<BitrixConfig>()
+                ?? throw new InvalidOperationException("Bitrix Config Not Found");
+            builder.Services.AddHttpClient<BitrixClient>(HttpClient =>
+            {
+                HttpClient.BaseAddress = new Uri(bitrixConfig.BaseAddress);
+            });
+
+            builder.Services.AddScoped<AuthService>();
+
+            builder.Services.AddScoped<BitrixService>();
+
+            builder.Services.AddScoped<IODataService, ODataService>();
+
+            builder.Services.AddScoped<ISaleDocService, SaleDocService>();
+
+            builder.Services.AddScoped<ISaleDocLogService, SaleDocLogService>();
+
+            builder.Services.AddHostedService<RabbitMqConsumer>();
 
             var app = builder.Build();
 
