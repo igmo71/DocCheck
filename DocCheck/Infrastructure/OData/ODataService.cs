d935274f968649da0c911be09dc7f8f86febd55d
Igor Mogilnitskiy <igmo71@outlook.com>
2025-09-05 15:26:12 +0300

    757 Remove GetListDocument_СчетФактураВыданный_ДокументыОснования.

--- DocCheck/Infrastructure/OData/ODataService.cs
+++ DocCheck/Infrastructure/OData/ODataService.cs
@@ -4,37 +4,14 @@ namespace DocCheck.Infrastructure.OData
 {
     public interface IODataService
     {
-        Task<Document_РеализацияТоваровУслуг?> GetDocument_РеализацияТоваровУслуг(string refKey);
-        Task<Document_РеализацияТоваровУслуг_Товары[]?> GetDocument_РеализацияТоваровУслуг_Товары(string refKey);
         Task<Document_СчетФактураВыданный?> GetDocument_СчетФактураВыданный(string refKey);
         Task<Document_СчетФактураВыданный?> GetDocument_СчетФактураВыданный_ByBaseDoc(string documentSaleRefKey);
-
-        //Task<Document_СчетФактураВыданный_ДокументыОснования[]?> GetListDocument_СчетФактураВыданный_ДокументыОснования(string? refKey);
+        Task<Document_РеализацияТоваровУслуг?> GetDocument_РеализацияТоваровУслуг(string refKey);
+        Task<Document_РеализацияТоваровУслуг_Товары[]?> GetDocument_РеализацияТоваровУслуг_Товары(string refKey);
     }
 
     public class ODataService(ODataClient oDataClient) : IODataService
     {
-        public async Task<Document_РеализацияТоваровУслуг?> GetDocument_РеализацияТоваровУслуг(string refKey)
-        {
-            var uri = Document_РеализацияТоваровУслуг.GetUri(refKey);
-
-            var rootobject = await oDataClient.GetDataAsync<Rootobject<Document_РеализацияТоваровУслуг>>(uri);
-
-            var result = rootobject?.Value?.FirstOrDefault();
-
-            return result;
-        }
-
-        public async Task<Document_РеализацияТоваровУслуг_Товары[]?> GetDocument_РеализацияТоваровУслуг_Товары(string refKey)
-        {
-            var uri = Document_РеализацияТоваровУслуг_Товары.GetUri(refKey);
-
-            var rootobject = await oDataClient.GetDataAsync<Rootobject<Document_РеализацияТоваровУслуг_Товары>>(uri);
-
-            var result = rootobject?.Value;
-
-            return result;
-        }
 
         public async Task<Document_СчетФактураВыданный?> GetDocument_СчетФактураВыданный(string refKey)
         {
@@ -58,18 +35,26 @@ namespace DocCheck.Infrastructure.OData
             return result;
         }
 
-        //public async Task<Document_СчетФактураВыданный_ДокументыОснования[]?> GetListDocument_СчетФактураВыданный_ДокументыОснования(string? refKey)
-        //{
-        //    if (refKey is null)
-        //        return null;
+        public async Task<Document_РеализацияТоваровУслуг?> GetDocument_РеализацияТоваровУслуг(string refKey)
+        {
+            var uri = Document_РеализацияТоваровУслуг.GetUri(refKey);
 
-        //    var uri = Document_СчетФактураВыданный_ДокументыОснования.GetUri(refKey);
+            var rootobject = await oDataClient.GetDataAsync<Rootobject<Document_РеализацияТоваровУслуг>>(uri);
 
-        //    var rootobject = await oDataClient.GetDataAsync<Rootobject<Document_СчетФактураВыданный_ДокументыОснования>>(uri);
+            var result = rootobject?.Value?.FirstOrDefault();
 
-        //    var result = rootobject?.Value;
+            return result;
+        }
 
-        //    return result;
-        //}
+        public async Task<Document_РеализацияТоваровУслуг_Товары[]?> GetDocument_РеализацияТоваровУслуг_Товары(string refKey)
+        {
+            var uri = Document_РеализацияТоваровУслуг_Товары.GetUri(refKey);
+
+            var rootobject = await oDataClient.GetDataAsync<Rootobject<Document_РеализацияТоваровУслуг_Товары>>(uri);
+
+            var result = rootobject?.Value;
+
+            return result;
+        }
     }
 }
