14846d24fd5861b4da7e95dbdf9e24bda026130c
Igor Mogilnitskiy <igmo71@outlook.com>
2025-09-15 11:42:10 +0300

    75783 SearchParams SaleDocNumber and BaseDocNumber.

--- DocCheck/Common/SearchParams.cs
+++ DocCheck/Common/SearchParams.cs
@@ -3,5 +3,7 @@
     public class SearchParams
     {
         public string? Customer { get; set; }
+        public string? SaleDocNumber { get; set; }
+        public string? BaseDocNumber { get; set; }
     }
 }
